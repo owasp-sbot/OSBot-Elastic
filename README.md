f967ec9bc16e7cc5a4ad256c4c6b14f28b241456
Dinis Cruz <dinis.cruz@owasp.org>
2020-12-31 12:27:19 +0000

    first commit of refactored OSBot-Elastic API

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # OSBot-Elastic
+
+
+## creating dev environment
+
+```
+python3 -m venv venv
+source venv/bin/activate
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -e .
+```
---
+++ osbot_elastic/Env.py
@@ -0,0 +1,16 @@
+from os import environ
+
+from dotenv import load_dotenv
+
+class Env():
+    def __init__(self):
+        load_dotenv()
+
+    def get_elastic_server_config(self):
+        return {
+                    "host"    : environ.get('ELASTIC_SERVER'),
+                    "kibana"  : environ.get('ELASTIC_KIBANA'),
+                    "port"    : environ.get('ELASTIC_PORT'),
+                    "username": environ.get('ELASTIC_USERNAME'),
+                    "password": environ.get('ELASTIC_PASSWORD')
+                }
